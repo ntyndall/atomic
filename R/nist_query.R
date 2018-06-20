fd8d37ead8655587dbd5de7a75004342ab66fae0
Niall Tyndall <ntyndall@gmail.com>
2018-06-21 00:01:16 +0100

    Refactor and document NIST query

--- R/nist_query.R
+++ R/nist_query.R
@@ -1,33 +1,43 @@
 #' @title Get data
 #'
+#' @description A function that sends a post query to NIST to 
+#'  retrieve the HTTP response containing the level information
+#'  in the form of a table, nested in an XML nodeset.
+#'
+#' @param series A character string containing an ionic series
+#'  which is of the form \code{{Element} {Series}}, e.g. Co ii.
+#'
 #' @export
 
+
 nist_query <- function(series) {
-  series <- 'Co ii'
+
+  # Send POST request to NIST, all list information required.
   response <- httr::POST(
     url = 'https://physics.nist.gov/cgi-bin/ASD/energy1.pl',
     body =
       list(
-      encodedlist = 'XXT2',
-      spectrum = series,
-      submit = 'Retrieve Data',
-      units = '0',
-      format = '0',
-      output = '0',
-      page_size = '15',
-      multiplet_ordered = '0',
-      conf_out = 'on',
-      term_out = 'on',
-      level_out = 'on',
-      unc_out = '1',
-      j_out = 'on',
-      lande_out = 'on',
-      perc_out = 'on',
-      biblio = 'on',
-      temp = ''
-    ),
+        encodedlist = 'XXT2',
+        spectrum = series,
+        submit = 'Retrieve Data',
+        units = '0',
+        format = '0',
+        output = '0',
+        page_size = '15',
+        multiplet_ordered = '0',
+        conf_out = 'on',
+        term_out = 'on',
+        level_out = 'on',
+        unc_out = '1',
+        j_out = 'on',
+        lande_out = 'on',
+        perc_out = 'on',
+        biblio = 'on',
+        temp = ''
+      ),
     encode = 'form'
   )
 
-  return(response$content %>% rawToChar())
+  # Return char response
+  return(rawToChar(response$content)) 
 }
