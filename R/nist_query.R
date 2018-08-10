e7458001ddc926bd377f6c2858d9b760130a752a
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-10 23:32:33 +0100

    Update the query for additional kwargs

--- R/nist_query.R
+++ R/nist_query.R
@@ -12,29 +12,33 @@
 
 nist_query <- function(series, INPUT) {
 
+  # Set up body args here
+  bodyArgs <-  list(
+    encodedlist = 'XXT2',
+    spectrum = series,
+    submit = 'Retrieve Data',
+    units = INPUT$conversion %>% as.character,
+    format = '0',
+    output = '0',
+    page_size = '15',
+    multiplet_ordered = '0',
+    conf_out = 'on',
+    term_out = 'on',
+    level_out = 'on',
+    unc_out = '1',
+    lande_out = 'on',
+    perc_out = 'on',
+    biblio = 'on',
+    temp = ''
+  )
+
+  # If LS is false, add fine structure levels on
+  if (INPUT$ls %>% `!`()) bodyArgs %<>% c(list(j_out = "on"))
+
   # Send POST request to NIST, all list information required.
   response <- httr::POST(
     url = 'https://physics.nist.gov/cgi-bin/ASD/energy1.pl',
-    body =
-      list(
-        encodedlist = 'XXT2',
-        spectrum = series,
-        submit = 'Retrieve Data',
-        units = INPUT$conversion %>% as.character,
-        format = '0',
-        output = '0',
-        page_size = '15',
-        multiplet_ordered = '0',
-        conf_out = 'on',
-        term_out = 'on',
-        level_out = 'on',
-        unc_out = '1',
-        j_out = 'on',
-        lande_out = 'on',
-        perc_out = 'on',
-        biblio = 'on',
-        temp = ''
-      ),
+    body = bodyArgs,
     encode = 'form'
   )
 
